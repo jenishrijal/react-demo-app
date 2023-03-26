ce10b98928ce1b3955d26ff4cd0589c9d50c9657
Jenish Rijal <zenishrz@gmail.com>
2023-03-26 13:13:53 +0545

    added input space username and password.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [step, setStep] = useState(0);
+
+  const handleReset = () => {
+    setUsername("");
+    setPassword("");
+    setStep(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" value={username} class="form-control" id="InputUsername" placeholder="Enter username" onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div class="form-group">
+        <label for="Password1">Password</label>
+        <input type="password" value={password} class="form-control" id="InputPassword1" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      </div>
+    </>
   );
 }
 
